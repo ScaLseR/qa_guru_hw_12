067a2c1e72f007d6beaa992f42fe25e4818bde1a
a.skorik <a.skorik@petrovich.ru>
2023-10-26 17:16:15 +0300

    edited requirements.txt, conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,49 +1,9 @@
 """фикстуры используемые для тестирования формы
 https://demoqa.com/automation-practice-form"""
-# import pytest
-# from selene.support.shared import browser
-
-
-# @pytest.fixture(scope='function', autouse=True)
-# def browser_setup():
-#     """Фикстура настройки браузера"""
-#     browser.config.window_width = 1920
-#     browser.config.window_height = 1080
-#     browser.config.timeout = 2.0
-#     browser.config.base_url = 'https://demoqa.com'
-
-# import pytest
-#
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selene import Browser, Config
-#
-# from utils import attach
-
-
-# @pytest.fixture(scope='function', autouse=True)
-# def browser_setup():
-#     """Фикстура настройки браузера"""
-#     browser.config.window_width = 1920
-#     browser.config.window_height = 1080
-#     browser.config.timeout = 2.0
-#     # browser.config.base_url = 'https://demoqa.com'
-#
-#     yield browser
-#
-#     attach.add_screenshot(browser)
-#     attach.add_logs(browser)
-#     attach.add_html(browser)
-#     attach.add_video(browser)
-#
-#     browser.quit()
-
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
-
 from utils import attach
 
 
@@ -59,10 +19,7 @@ def setup_browser(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
+    driver = webdriver.Remote(command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub", options=options)
 
     browser = Browser(Config(driver))
     yield browser
